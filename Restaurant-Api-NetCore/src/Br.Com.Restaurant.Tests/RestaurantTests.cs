4ae42ae1260da09754434ff5c09ed4a4d9c1ed67
Daniel Zat <Daniel Zat@DANIELZAT>
2020-08-19 23:06:46 -0300

    Alterado git ignore para ignorar itens do projeto net core e iniciada a implementação de testes

--- Restaurant-Api-NetCore/src/Br.Com.Restaurant.Tests/RestaurantTests.cs
+++ Restaurant-Api-NetCore/src/Br.Com.Restaurant.Tests/RestaurantTests.cs
@@ -1,6 +1,56 @@
-﻿namespace Br.Com.Restaurant.Tests
+﻿using Br.Com.Restaurant.Business.Interfaces;
+using Br.Com.Restaurant.Business.Services;
+using Moq;
+using System.Linq;
+using Xunit;
+
+namespace Br.Com.Restaurant.Tests
 {
+    [Collection(nameof(RestaurantCollection))]
     public class RestaurantTests
     {
+        private readonly RestaurantTestsFixture _restaurantTestsFixture;
+
+        public RestaurantTests(RestaurantTestsFixture restaurantTestsFixture)
+        {
+            _restaurantTestsFixture = restaurantTestsFixture;
+        }
+
+        [Fact(DisplayName = "Must return restaurants")]
+        [Trait("Category", "Restaurant")]
+        public async void Restaurant_GetRestaurants_MustReturnRestaurants()
+        {
+            //Arrange
+            var restaurantRepository = new Mock<IRestaurantRepository>();
+            var restaurantService = new RestaurantService(restaurantRepository.Object);
+
+            restaurantRepository.Setup(f => f.GetAll("nome")).Returns(_restaurantTestsFixture.GenerateRestaurants(100));
+
+            //Act
+            var restaurants = await restaurantService.GetRestaurants("");
+
+            //Assert
+            restaurantRepository.Verify(f => f.GetAll("nome"), Times.Once);
+            Assert.True(restaurants.Any());
+        }
+
+        [Fact(DisplayName = "Must return restaurant")]
+        [Trait("Category", "Restaurant")]
+        public async void Restaurant_GetRestaurantById_MustReturnRestaurant()
+        {
+            //Arrange
+            var restaurantRepository = new Mock<IRestaurantRepository>();
+            var restaurantService = new RestaurantService(restaurantRepository.Object);
+
+            restaurantRepository.Setup(f => f.GetRestaurantById("123")).Returns(_restaurantTestsFixture.GenerateRestaurant("123"));
+
+            //Act
+            var restaurant = await restaurantService.GetRestaurants("123");
+
+            //Assert
+            restaurantRepository.Verify(f => f.GetRestaurantById("123"), Times.Once);
+            Assert.True(restaurant != null);
+        }
+
     }
 }
--- Restaurant-Api-NetCore/src/Br.Com.Restaurant.Tests/RestaurantTestsFixture.cs
+++ Restaurant-Api-NetCore/src/Br.Com.Restaurant.Tests/RestaurantTestsFixture.cs
@@ -1,6 +1,46 @@
-﻿namespace Br.Com.Restaurant.Tests
+﻿using Bogus;
+using Br.Com.Restaurant.Business.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Br.Com.Restaurant.Tests
 {
-    public class RestaurantTestsFixture
+
+    [CollectionDefinition(nameof(RestaurantCollection))]
+    public class RestaurantCollection : ICollectionFixture<RestaurantTestsFixture>
+    { }
+
+    public class RestaurantTestsFixture : IDisposable
     {
+        public Task<List<Restaurants>> GenerateRestaurants(int quantity)
+        {
+            var restaurants = new Faker<Restaurants>("pt_BR")
+                .CustomInstantiator(f => new Restaurants(
+                    
+            //TODO: Implementar criação de dados fake do restaurante 
+                
+            ));
+
+            return Task.FromResult(restaurants.Generate(quantity));
+        }
+
+        public Task<Restaurants> GenerateRestaurant(string objectId)
+        {
+            var restaurant = new Faker<Restaurants>("pt_BR")
+                .CustomInstantiator(f => new Restaurants(
+
+            //TODO: Implementar criação de dados fake do restaurante 
+
+            ));
+
+            return Task.FromResult(restaurant.Generate(1).FirstOrDefault());
+        }
+
+        public void Dispose()
+        {
+        }
     }
 }
